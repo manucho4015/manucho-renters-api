6a9c80fc1a1217fad341a981a27cf8060076b9b6
Emmanuel Ochieng <eochieng16@gmail.com>
2022-10-11 18:36:10 +0300

    add landlord authentication middleware

---
+++ middleware/auth-landlord.js
@@ -0,0 +1,27 @@
+const Landlord = require("../models/Landlord");
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+const { StatusCodes } = require("http-status-codes");
+
+const auth = (req, res, next) => {
+  //check header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError(StatusCodes.UNAUTHORIZED, "No valid token");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userId, name: payload.name };
+    next();
+  } catch (error) {
+    throw new CustomAPIError(
+      StatusCodes.UNAUTHORIZED,
+      "Authentication invalid"
+    );
+  }
+};
+
+module.exports = auth;
