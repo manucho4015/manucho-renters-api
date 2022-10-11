912ff56feac924e3a4aaea7d6a92aec7fdd5cd5a
Emmanuel Ochieng <eochieng16@gmail.com>
2022-10-11 19:46:18 +0300

    update Apartment model

--- models/Apartment.js
+++ models/Apartment.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const LandlordSchema = new mongoose.Schema(
+const ApartmentSchema = new mongoose.Schema(
   {
     name: {
       type: String,
@@ -12,6 +12,11 @@ const LandlordSchema = new mongoose.Schema(
       required: [true, "Please provide landlord"],
       maxlength: 20,
     },
+    tenants: {
+      type: Number,
+      required: [true, "Please provide number of tenants"],
+      maxlength: 8,
+    },
     expected_income: {
       type: Number,
       required: [true, "Please provide phone expected income"],
@@ -21,4 +26,4 @@ const LandlordSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Landlord", LandlordSchema);
+module.exports = mongoose.model("Apartment", ApartmentSchema);
