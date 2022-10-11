367f4512cea2adf3c784675e0d8285e90c3de506
Emmanuel Ochieng <eochieng16@gmail.com>
2022-10-11 18:39:00 +0300

    generate token on landlord login

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,9 @@ const { StatusCodes } = require("http-status-codes");
 const unirest = require("unirest");
 
 const CustomAPIError = require("../errors/custom-error");
+const Landlord = require("../models/Landlord");
+const Caretaker = require("../models/Caretaker");
+const Tenant = require("../models/Tenant");
 
 const loginOwner = async (request, response) => {
   let res_body = {};
@@ -34,6 +37,76 @@ const loginOwner = async (request, response) => {
     });
 };
 
+const loginLandlord = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomAPIError(
+      StatusCodes.BAD_REQUEST,
+      "Please provide both email and password"
+    );
+  }
+  const landlord = await Landlord.findOne({ email });
+
+  if (!landlord) {
+    throw new CustomAPIError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  const isPasswordCorrect = await landlord.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomAPIError(StatusCodes.UNAUTHORIZED, "Invalid Password");
+  }
+
+  const token = landlord.createJWT();
+
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, message: "logged in!", token });
+};
+
+const loginCaretaker = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomAPIError(
+      StatusCodes.BAD_REQUEST,
+      "Please provide both email and password"
+    );
+  }
+  const caretaker = await Caretaker.findOne({ email });
+  if (!caretaker) {
+    throw new CustomAPIError(StatusCodes.UNAUTHORIZED, "User not found");
+  }
+  const isPasswordCorrect = await caretaker.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomAPIError(StatusCodes.UNAUTHORIZED, "Invalid Password");
+  }
+
+  res.status(StatusCodes.OK).json({ success: true, status: "logged in!" });
+};
+const loginTenant = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomAPIError(
+      StatusCodes.BAD_REQUEST,
+      "Please provide both email and password"
+    );
+  }
+  const tenant = await Tenant.findOne({ email });
+  if (!tenant) {
+    throw new CustomAPIError(StatusCodes.UNAUTHORIZED, "User not found");
+  }
+  const isPasswordCorrect = await tenant.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomAPIError(StatusCodes.UNAUTHORIZED, "Invalid Password");
+  }
+
+  res.status(StatusCodes.OK).json({ success: true, status: "logged in!" });
+};
+
 module.exports = {
   loginOwner,
+  loginLandlord,
+  loginCaretaker,
+  loginTenant,
 };
